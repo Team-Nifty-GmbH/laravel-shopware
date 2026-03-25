9db44e6e71b76bb5eb86205446773db4ea1c2cb1
patrickweh <pw@team-nifty.com>
2026-03-25 08:28:18 +0100

    Auto-authenticate connector on creation

--- src/ShopwareServiceProvider.php
+++ src/ShopwareServiceProvider.php
@@ -21,13 +21,17 @@ class ShopwareServiceProvider extends ServiceProvider
         $this->mergeConfigFrom(__DIR__ . '/../config/shopware.php', 'shopware');
 
         $this->app->singleton(Shopware::class, function ($app) {
-            return new Shopware(
+            $connector = new Shopware(
                 baseUrl: config('shopware.base_url'),
                 clientId: config('shopware.client_id'),
                 clientSecret: config('shopware.client_secret'),
                 tokenUrl: config('shopware.token_url'),
                 scopes: config('shopware.scopes'),
             );
+
+            $connector->authenticate($connector->getAccessToken());
+
+            return $connector;
         });
 
         $this->app->alias(Shopware::class, 'shopware');
